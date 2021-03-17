194469e457aa2210e2ee603a9ab6fbb928180a72
prajotsurey <prajotsurey@gmail.com>
2021-03-17 20:36:45 +0530

    asd

---
+++ C/library-frontend/src/queries.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client'
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+      id
+    }
+  }
+`
+
+
+export const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
+
+
+export const CREATE_BOOK = gql`
+  mutation createBook ($title: String!, $author: String! , $published: String!, $genres: [String!]) {
+    addBook (
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    ) {
+      title
+      author
+      published
+      genres
+    }
+  }
+`
+
+export const UPDATE_AUTHOR = gql`
+  mutation updateAuthor( $name: String!, $born: String!) {
+    editAuthor (
+      name: $name,
+      setBornTo: $born
+    ) {
+      name
+      born
+      bookCount
+    }
+  }
+`
